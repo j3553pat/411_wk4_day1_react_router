5b779f5a2ee2e4b3049f75acb6a477e3b4a75e44
j3553pat <jessecodedev@gmail.com>
2023-06-05 19:16:18 -0500

    Updates

---
+++ precache-manifest.d160eec0c7c3c8510496dabd58fe20bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7436513995739a3401fe39f07e29319",
+    "url": "http//:j3553pat.github.io/411_wk4_day1_react_router/index.html"
+  },
+  {
+    "revision": "63331426df43ceee5df2",
+    "url": "http//:j3553pat.github.io/411_wk4_day1_react_router/static/css/main.8e4de9ae.chunk.css"
+  },
+  {
+    "revision": "d85c85e7b903694a631d",
+    "url": "http//:j3553pat.github.io/411_wk4_day1_react_router/static/js/2.7c863ec5.chunk.js"
+  },
+  {
+    "revision": "63331426df43ceee5df2",
+    "url": "http//:j3553pat.github.io/411_wk4_day1_react_router/static/js/main.aeca291f.chunk.js"
+  },
+  {
+    "revision": "3e3f16356f1c8b86064a",
+    "url": "http//:j3553pat.github.io/411_wk4_day1_react_router/static/js/runtime~main.11ee355a.js"
+  }
+]);
